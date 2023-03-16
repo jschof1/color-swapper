38666e3bbcac2a3d9a985f85bbfb168ec6e98be5
jschof1 <59843981+jschof1@users.noreply.github.com>
2023-03-16 15:19:28 +0000

    3 files modified

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-// Import stylesheets
 import './style.css';
-
-const inputImage = document.getElementById('inputImage');
+const inputImages = document.getElementById('inputImages');
 const sourceColor = document.getElementById('sourceColor');
 const targetColor = document.getElementById('targetColor');
-const processImage = document.getElementById('processImage');
-const imageCanvas = document.getElementById('imageCanvas');
-const downloadImage = document.getElementById('downloadImage');
-const ctx = imageCanvas.getContext('2d');
+const processImages = document.getElementById('processImages');
+const imageContainer = document.getElementById('imageContainer');
+const downloadImages = document.getElementById('downloadImages');
 
-inputImage.addEventListener('change', (e) => {
-  const img = new Image();
-  img.src = URL.createObjectURL(e.target.files[0]);
-  img.onload = () => {
-    imageCanvas.width = img.width;
-    imageCanvas.height = img.height;
-    ctx.drawImage(img, 0, 0, img.width, img.height);
-  };
+inputImages.addEventListener('change', (e) => {
+  imageContainer.innerHTML = '';
+  for (const file of e.target.files) {
+    const img = new Image();
+    img.src = URL.createObjectURL(file);
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      canvas.width = img.width;
+      canvas.width = img.width;
+      canvas.height = img.height;
+      canvas.style.border = '1px solid';
+      canvas.style.margin = '5px';
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(img, 0, 0, img.width, img.height);
+      imageContainer.appendChild(canvas);
+    };
+  }
 });
 
 function colorDistanceSquared(color1, color2) {
-  return Math.pow(color1.r - color2.r, 2) + Math.pow(color1.g - color2.g, 2) + Math.pow(color1.b - color2.b, 2);
+  return (
+    Math.pow(color1.r - color2.r, 2) +
+    Math.pow(color1.g - color2.g, 2) +
+    Math.pow(color1.b - color2.b, 2)
+  );
 }
 
-function replaceColor(source, target, tolerance = 32) {
-  const imgData = ctx.getImageData(0, 0, imageCanvas.width, imageCanvas.height);
+function replaceColor(canvas, source, target, tolerance = 32) {
+  const ctx = canvas.getContext('2d');
+  const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
   for (let i = 0; i < imgData.data.length; i += 4) {
     const r = imgData.data[i];
@@ -52,15 +63,22 @@ function hexToRgb(hex) {
     : null;
 }
 
-processImage.addEventListener('click', () => {
+processImages.addEventListener('click', () => {
   const source = hexToRgb(sourceColor.value);
   const target = hexToRgb(targetColor.value);
-  replaceColor(source, target);
+  const canvases = imageContainer.getElementsByTagName('canvas');
+  for (const canvas of canvases) {
+    replaceColor(canvas, source, target);
+  }
 });
 
-downloadImage.addEventListener('click', () => {
-  const link = document.createElement('a');
-  link.download = 'processed_image.png';
-  link.href = imageCanvas.toDataURL('image/png');
-  link.click();
+downloadImages.addEventListener('click', () => {
+  const canvases = imageContainer.getElementsByTagName('canvas');
+  const canvasArray = Array.from(canvases); // Convert the HTMLCollection to an array
+  for (const [index, canvas] of canvasArray.entries()) {
+    const link = document.createElement('a');
+    link.download = `processed_image_${index + 1}.png`;
+    link.href = canvas.toDataURL('image/png');
+    link.click();
+  }
 });
